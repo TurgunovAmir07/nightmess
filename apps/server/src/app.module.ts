316f851db5e7668fe08c890c1dffe3990752e62b
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-06-10 01:26:57 +0300

    base

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { EnvConfigOptions } from './configs'
 
 @Module({
-  imports: [],
+	imports: [ConfigModule.forRoot(EnvConfigOptions)]
 })
 export class AppModule {}
