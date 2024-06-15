bc9dc717f0abd7ee31cd31a1f5be1860483ba912
Amir <turgunovamir9@gmail.com>
2024-06-15 15:54:36 +0800

    add types for settings .data

--- apps/client/src/widgets/Settings/model/data/settingsFields.data.ts
+++ apps/client/src/widgets/Settings/model/data/settingsFields.data.ts
@@ -1,4 +1,27 @@
-export const settingsFieldsArr = [
+interface TRules {
+	required: string
+	pattern: {
+		value: RegExp
+		message: string
+	}
+}
+
+type TInputType = 'text' | 'email' | 'tel' | 'radio'
+
+interface TSettingsFields {
+	id?: string
+	value?: string
+	description?: string
+	isRadio?: boolean
+	name?: string
+	type: TInputType
+	placeholder?: string
+	label: string
+	rules?: TRules
+	img?: string
+}
+
+export const settingsFieldsArr: TSettingsFields[] = [
 	{
 		name: 'email',
 		type: 'email',
