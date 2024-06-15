ae7eb1ae867d5288bf1776cc288c779b3d1d6b1a
Amir <turgunovamir9@gmail.com>
2024-06-15 21:59:54 +0800

    create gotogamepage button

--- apps/client/src/widgets/Header/ui/Header.tsx
+++ apps/client/src/widgets/Header/ui/Header.tsx
@@ -1,8 +1,8 @@
 import cl from './Header.module.scss'
 import { ProfileAuthButton } from '@/features/ProfileAuthButton'
 import { CartButton } from '@/features/CartButton'
-import { GamepadButton } from '@/features/GamepadButton'
 import { GoToMapPageButton } from '@/features/GoToMapPageButton'
+import { GoToGamePage } from '@/features/GoToGamePageButton'
 
 export const Header = ({
 	title,
@@ -14,7 +14,7 @@ export const Header = ({
 	return (
 		<div className={cl.root}>
 			<div className={cl.root__header}>
-				{isMap ? <GamepadButton /> : <GoToMapPageButton />}
+				{isMap ? <GoToGamePage /> : <GoToMapPageButton />}
 				<h1 className={cl.root__header__title}>{title}</h1>
 				<ProfileAuthButton />
 			</div>
