c176bec52ec0c442f1dc586455bc8e279b5c5d04
Amir <turgunovamir9@gmail.com>
2024-06-17 11:34:14 +0800

    small fix for topbar

---
+++ apps/client/src/pages/Topbar/model/data/topbar.data.ts
@@ -0,0 +1,15 @@
+interface TTopbarData {
+	path: string
+	label: string
+}
+
+export const topbarData: TTopbarData[] = [
+	{
+		path: '/game',
+		label: 'GAME'
+	},
+	{
+		path: '/profile',
+		label: 'PROFILE'
+	}
+]
--- apps/client/src/pages/Topbar/ui/Topbar.tsx
+++ apps/client/src/pages/Topbar/ui/Topbar.tsx
@@ -2,6 +2,7 @@ import { Link, Navigate, Outlet, useLocation } from 'react-router-dom'
 import cl from './Topbar.module.scss'
 import { useEffect, useState } from 'react'
 import { Container } from '@/shared'
+import { topbarData } from '../model/data/topbar.data'
 
 export const Topbar = () => {
 	const location = useLocation()
@@ -16,7 +17,7 @@ export const Topbar = () => {
 	return (
 		<Container>
 			<div className={cl.root}>
-				<div className={cl.root__container}>
+				<nav className={cl.root__container}>
 					<span
 						className={`${cl.root__container_separator} ${
 							activePath === '/game'
@@ -25,45 +26,31 @@ export const Topbar = () => {
 						}
             `}
 					/>
-					<Link to='/game' className={cl.root__container__item}>
-						<label className={cl.root__container__item_label}>
-							<input
-								className={
-									cl.root__container__item_label__radio
-								}
-								type='radio'
-								name='topbar'
-							/>
-							<span
-								className={cl.root__container__item_label__text}
-							>
-								GAME
-							</span>
-						</label>
-					</Link>
-					<Link
-						to='/profile'
-						className={`${cl.root__container__item} ${
-							activePath === '/profile' &&
-							cl.root__container__item_active
-						}`}
-					>
-						<label className={cl.root__container__item_label}>
-							<input
-								className={
-									cl.root__container__item_label__radio
-								}
-								type='radio'
-								name='topbar'
-							/>
-							<span
-								className={cl.root__container__item_label__text}
-							>
-								PROFILE
-							</span>
-						</label>
-					</Link>
-				</div>
+					{topbarData.map((item, index) => (
+						<Link
+							key={index}
+							to={item.path}
+							className={cl.root__container__item}
+						>
+							<label className={cl.root__container__item_label}>
+								<input
+									className={
+										cl.root__container__item_label__radio
+									}
+									type='radio'
+									name='topbar'
+								/>
+								<span
+									className={
+										cl.root__container__item_label__text
+									}
+								>
+									{item.label}
+								</span>
+							</label>
+						</Link>
+					))}
+				</nav>
 			</div>
 			<Outlet />
 		</Container>
