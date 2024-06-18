b264c22bab3a751f9978c6180fa608c8d4464588
Amir <turgunovamir9@gmail.com>
2024-06-18 22:33:47 +0800

    small fix

--- apps/client/src/shared/ui/container/Container.tsx
+++ apps/client/src/shared/ui/container/Container.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import { ReactNode } from 'react'
 import cl from './Container.module.scss'
 
-export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <div className={cl.root}>{children}</div>
+export const Container = ({ children }: { children: ReactNode }) => {
+	return <div className={cl.root}>{children}</div>
 }
