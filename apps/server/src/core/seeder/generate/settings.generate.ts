5c96bbcac16091abf391b94b19e33edd66210635
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-06-19 22:14:00 +0300

    add setting

--- apps/server/src/core/seeder/generate/settings.generate.ts
+++ apps/server/src/core/seeder/generate/settings.generate.ts
@@ -42,6 +42,24 @@ export const settingsGenerate = (i: ESettingsName) => {
 				setting.value = '1'
 			}
 			break
+		case ESettingsName.CRAFT_NULL_CHANCE:
+			{
+				setting.description = 'Шанс крафта с 0 на 1 уровень'
+				setting.value = '80'
+			}
+			break
+		case ESettingsName.CRAFT_ONE_CHANCE:
+			{
+				setting.description = 'Шанс крафта с 1 на 2 уровень'
+				setting.value = '60'
+			}
+			break
+		case ESettingsName.CRAFT_TWO_CHANCE:
+			{
+				setting.description = 'Шанс крафта с 2 на 3 уровень'
+				setting.value = '40'
+			}
+			break
 	}
 
 	return setting
