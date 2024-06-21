84474f729ce05b8de69fe3d10805adff9514f0b9
Amir <turgunovamir9@gmail.com>
2024-06-22 01:21:11 +0800

    add items to miniinventory

--- apps/client/src/widgets/MiniInventory/ui/MiniInventory.tsx
+++ apps/client/src/widgets/MiniInventory/ui/MiniInventory.tsx
@@ -1,9 +1,22 @@
 import cl from './MiniInventory.module.scss'
 import { MiniInventoryItem } from '.'
-import { miniInventoryData } from '../model/data/miniInventory.data'
 import { OpenInventory } from '@/features/OpenInventory'
+import { TCards, useGetInventoryQuery } from '@/store'
+import { useEffect, useState } from 'react'
+import { sorterItems } from '../lib/sorterItems'
 
 export const MiniInventory = () => {
+	const { data } = useGetInventoryQuery()
+
+	const [sortedItems, setSortedItems] = useState<(TCards | null)[]>([])
+
+	useEffect(() => {
+		if (data && data.cards) {
+			const sorted = sorterItems([...data.cards]).slice(0, 4)
+			setSortedItems(sorted)
+		}
+	}, [data])
+
 	return (
 		<div className={cl.root}>
 			<div className={cl.root__wrap}>
@@ -17,14 +30,16 @@ export const MiniInventory = () => {
 				<span className={cl.root__wrap_text}>
 					<OpenInventory />
 				</span>
-				{miniInventoryData.map((item, index) => (
-					<div className={cl.root__wrap__frame} key={index}>
-						<MiniInventoryItem
-							isEmpty={item.isEmpty}
-							count={item.count}
-						/>
-					</div>
-				))}
+				{data?.cards &&
+					sortedItems.map((item, index) => (
+						<div className={cl.root__wrap__frame} key={index}>
+							<MiniInventoryItem
+								count={item && item.count}
+								src={item && item.card.miniature}
+								alt={item ? item.card.name : ''}
+							/>
+						</div>
+					))}
 			</div>
 		</div>
 	)
