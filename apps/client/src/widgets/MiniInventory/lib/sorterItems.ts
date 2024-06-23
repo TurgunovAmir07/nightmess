cc7d1c37285f7d17365bce1302acce65bee08f25
Amir <turgunovamir9@gmail.com>
2024-06-23 18:50:57 +0800

    delete unusual component

--- apps/client/src/widgets/MiniInventory/lib/sorterItems.ts
+++
@@ -1,9 +0,0 @@
-import { colorOrder } from '@/shared'
-import type { TCards } from '@/store'
-
-export const sorterItems = (cards: TCards[]): (TCards | null)[] => {
-	return cards.sort(
-		(a, b) =>
-			colorOrder.indexOf(b.card.color) - colorOrder.indexOf(a.card.color)
-	)
-}
